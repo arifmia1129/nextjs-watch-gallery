4cce333bd16321510359ac630eb2771a68516828
arifmia1129 <arifmia1129@gmail.com>
2023-08-24 14:51:27 +0600

    if we want create custom 404 so we should create not-found file into app folder

---
+++ src/app/not-found.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div className="flex h-screen justify-center items-center">
+      <h1 className="text-7xl font-bold">404!!! Page Not Found</h1>
+    </div>
+  );
+};
+
+export default NotFound;
